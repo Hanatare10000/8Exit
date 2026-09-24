9bb5d4bb355216fc93fe9bbafd6bd1ad7379062f
Tagawa Miduki <kmm2659345@stu.o-hara.ac.jp>
2026-09-24 15:10:04 +0900

    僕ら未完成

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,5 +1,3 @@
-using TMPro.EditorUtilities;
-using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
--- Assets/Up.cs
+++ Assets/Up.cs
@@ -1,5 +1,4 @@
 using System.Data;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class Up : MonoBehaviour
